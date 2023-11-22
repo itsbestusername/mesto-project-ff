b007b8dc46442cc7889c83a41afcf00a55ca6bf9
Ekaterina Kim <im.work-k@mail.ru>
2023-11-22 10:52:18 +0300

    add cards on page

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,54 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardContainer = document.querySelector('.places__list');
+const cards = cardContainer.querySelectorAll('.card');
+const deleteButton = document.querySelector('.card__delete-button');
+const likeButton = document.querySelector('.card__like-button');
+const addButton = document.querySelector('.profile__add-button');
+
+function createCity(city) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = city.link;
+    cardElement.querySelector('.card__image').alt = city.name;
+    cardElement.querySelector('.card__title').textContent = city.name;
+
+    return cardElement;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const initialCards = [
+        {
+          name: "Архыз",
+          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+        },
+        {
+          name: "Челябинская область",
+          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+        },
+        {
+          name: "Иваново",
+          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+        },
+        {
+          name: "Камчатка",
+          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+        },
+        {
+          name: "Холмогорский район",
+          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+        },
+        {
+          name: "Байкал",
+          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+        }
+    ];
+
+    initialCards.forEach((city) => {
+        const card = createCity(city);
+        cardContainer.append(card);
+    });
+});
