4763a4f520f2850fdf7e5d246f2ad7a944f844eb
Ekaterina Kim <im.work-k@mail.ru>
2023-11-27 13:28:08 +0300

    change deleteButton function

--- scripts/index.js
+++ scripts/index.js
@@ -8,68 +8,58 @@
 
 // @todo: Вывести карточки на страницу
 
-const cardContainer = document.querySelector('.places__list');
-const cards = cardContainer.querySelectorAll('.card');
-const likeButton = document.querySelector('.card__like-button');
-const addButton = document.querySelector('.profile__add-button');
-const closeButton = document.querySelector('.popup_type_new-card .popup__close');
-const popupNewCard = document.querySelector('.popup_type_new-card');
+const cardContainer = document.querySelector(".places__list");
+const cards = cardContainer.querySelectorAll(".card");
+const cardTemplate = document.querySelector("#card-template").content;
+const addButton = document.querySelector(".profile__add-button");
+const closeButton = document.querySelector(".popup_type_new-card .popup__close");
+const popupNewCard = document.querySelector(".popup_type_new-card");
+const saveButton = document.querySelector(".button .popup__button");
 
-function createCity(city) {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+function createCard(card, handleDelete) {
+  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
 
-    cardElement.querySelector('.card__image').src = city.link;
-    cardElement.querySelector('.card__image').alt = city.name;
-    cardElement.querySelector('.card__title').textContent = city.name;
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
+  cardElement.querySelector(".card__title").textContent = card.name;
 
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', function () {
-        cardElement.remove();
-    });
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", handleDelete);
 
-    return cardElement;
+  return cardElement;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const initialCards = [
-        {
-          name: "Архыз",
-          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-        },
-        {
-          name: "Челябинская область",
-          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-        },
-        {
-          name: "Иваново",
-          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-        },
-        {
-          name: "Камчатка",
-          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-        },
-        {
-          name: "Холмогорский район",
-          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-        },
-        {
-          name: "Байкал",
-          link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-        }
-    ];
+function handleDelete(event) {
+  const card = event.target.closest(".card");
 
-    initialCards.forEach((city) => {
-        const card = createCity(city);
-        cardContainer.append(card);
-    });
+  card.remove();
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  initialCards.forEach((card) => {
+    const place = createCard(card, handleDelete);
+    cardContainer.append(place);
+  });
 });
 
-addButton.addEventListener('click', function () {
-
-  popupNewCard.classList.add('popup_is-opened');
+addButton.addEventListener("click", function () {
+  popupNewCard.classList.add("popup_is-opened");
 });
 
-closeButton.addEventListener('click', function () {
-  popupNewCard.classList.remove('popup_is-opened');
+closeButton.addEventListener("click", function () {
+  popupNewCard.classList.remove("popup_is-opened");
 });
+
+// saveButton.addEventListener("click", function () {
+//   let nameOfCard = document.querySelector(".popup__input_type_card-name").value;
+//   let linkOfCard = document.querySelector(".popup__input_type_url").value;
+
+//   if (nameOfCard && linkOfCard) {
+//     const newCard = createCard({ name: nameOfCard, link: linkOfCard });
+//   }
+
+//   cardContainer.append(newCard);
+
+//   nameOfCard = "";
+//   linkOfCard = "";
+// });
