bfd67a3dba0aba8b72bf6b88e720961ac0161fc1
Ekaterina Kim <im.work-k@mail.ru>
2023-12-20 18:45:15 +0300

    add feature: change value of profile name and job

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,3 @@
-
 // import '../pages/index.css';
 // import { initialCards } from './cards';
 
@@ -7,17 +6,26 @@ const cards = cardContainer.querySelectorAll(".card");
 const cardTemplate = document.querySelector("#card-template").content;
 
 const addButton = document.querySelector(".profile__add-button");
-const closeCreateButton = document.querySelector(".popup_type_new-card .popup__close");
+const closeCreateButton = document.querySelector(
+  ".popup_type_new-card .popup__close"
+);
 const popupNewCard = document.querySelector(".popup_type_new-card");
 
 const editButton = document.querySelector(".profile__edit-button");
 const popupEdit = document.querySelector(".popup_type_edit");
-const editCloseButton = document.querySelector(".popup_type_edit .popup__close");
+const editCloseButton = document.querySelector(
+  ".popup_type_edit .popup__close"
+);
+const nameEditForm = document.querySelector(".popup__input_type_name");
+const description = document.querySelector(".popup__input_type_description");
+const editForm = document.querySelector(".popup__form");
 
 const saveButton = document.querySelector(".button .popup__button");
 
 function createCard(card, handleDelete) {
-  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
 
   cardElement.querySelector(".card__image").src = card.link;
   cardElement.querySelector(".card__image").alt = card.name;
@@ -27,44 +35,81 @@ function createCard(card, handleDelete) {
   deleteButton.addEventListener("click", handleDelete);
 
   return cardElement;
-};
+}
 
 function handleDelete(event) {
   const card = event.target.closest(".card");
 
   card.remove();
-};
+}
 
-function addFirstCards () {
-    initialCards.forEach((card) => {
-      const place = createCard(card, handleDelete);
-      cardContainer.append(place);
-    });
-  };
+function addFirstCards() {
+  initialCards.forEach((card) => {
+    const place = createCard(card, handleDelete);
+    cardContainer.append(place);
+  });
+}
 
 function openWindow(popup) {
   popup.classList.add("popup_is-opened");
+  popup.classList.add("popup_is-animated");
 }
 
 function closeWindow(popup) {
   popup.classList.remove("popup_is-opened");
+  // document.removeEventListener('keydown', closeOnEsc);
 }
 
-addButton.addEventListener("click", function() {
+function closeOnEsc(evt) {
+  if (evt.key === "Escape") {
+    const popup = document.querySelector(".popup_is-opened");
+    closeWindow(popup);
+  }
+}
+
+//реализовать закрытие по клику на лэйаут
+// function closeOnLayout(evt) {
+//   if (evt.target.classList.contains(".popup_is-opened")) {
+//     closeWindow(popup);
+//   }
+// };
+
+// Обработчик «отправки» формы
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  let profileTitle = document.querySelector(".profile__title");
+  let profileDescription = document.querySelector(".profile__description");
+
+  profileTitle.textContent = nameEditForm.value;
+  profileDescription.textContent = description.value;
+
+  closeWindow(popupEdit);
+}
+
+editForm.addEventListener("submit", handleFormSubmit);
+
+addButton.addEventListener("click", () => {
   openWindow(popupNewCard);
 });
 
-closeCreateButton.addEventListener("click", function () {
+closeCreateButton.addEventListener("click", () => {
   closeWindow(popupNewCard);
 });
 
-editButton.addEventListener("click", function() {
+editButton.addEventListener("click", () => {
   openWindow(popupEdit);
+
+  nameEditForm.value = "Жак-Ив Кусто";
+  description.value = "Исследователь океана";
 });
 
-editCloseButton.addEventListener("click", function () {
+editCloseButton.addEventListener("click", () => {
   closeWindow(popupEdit);
 });
+
+document.addEventListener("keydown", closeOnEsc);
+// document.addEventListener('mousedown', closeOnLayout);
 
 // saveButton.addEventListener("click", function () {
 //   let nameOfCard = document.querySelector(".popup__input_type_card-name").value;
